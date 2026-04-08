8bb4fbb40ba12b1e2b76ec5acd4a793f17f7c03e
Jisko <sergio.jisko@gmail.com>
2026-04-09 03:15:34 +0700

    feat(dashboard): add ui/ barrel export

---
+++ packages/dashboard/src/components/ui/index.ts
@@ -0,0 +1,15 @@
+export { Button } from "./Button.js";
+export { Card } from "./Card.js";
+export { Chip, type ChipStatus } from "./Chip.js";
+export { Pill } from "./Pill.js";
+export { StatGrid, type StatItem } from "./StatGrid.js";
+export { ActivityItem, type ActivityTagVariant } from "./ActivityItem.js";
+export { Alert, type AlertVariant } from "./Alert.js";
+export { Input } from "./Input.js";
+export { Table, THead, TBody, TR, TH, TD } from "./Table.js";
+export { EmptyState } from "./EmptyState.js";
+export { Skeleton } from "./Skeleton.js";
+export { TextLink } from "./TextLink.js";
+export { EntityLink } from "./EntityLink.js";
+export { PageHeader } from "./PageHeader.js";
+export { SectionLabel } from "./SectionLabel.js";
